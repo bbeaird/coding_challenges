9289586ccfcc9aa757b168dcd74897f8d3c36ae0
Brantley Beaird <bbeaird@gmail.com>
2014-06-14 19:17:50 -0700

    Refactored...much better.

--- letter_changes.rb
+++ letter_changes.rb
@@ -1,28 +1,9 @@
-def LetterChanges(str)
-  letters_array = ('a'..'z').to_a
-  split_str = str.split('')
-  new_str = ""
-
-  split_str.each_with_index do |letter, i|
-    if letter =~ /[a-zA-Z]/
-      next_index = letters_array.index(letter.downcase) + 1
-      if letter.downcase == letter
-        new_str += letters_array[next_index]
-      elsif letter.upcase == letter
-        new_str += letters_array[next_index].upcase
-      end
-    else
-      new_str += letter
-    end
-  end
-  # vowel_upcase_hash = { 'a' => 'A', 'e' => 'E', 'i' => 'I', 'o' => 'O', 'u' => 'U' }
-  new_str = new_str.split('')
-  new_str.each_with_index do |letter, i|
-    if letter =~ /[aeiou]/
-      new_str[i] = letter.upcase
-    end
-  end
-  return new_str.join('')
+def letter_changes(str)
+  str.split('').map do |char|
+    char.next! if char =~ /[a-zA-Z]/
+    char.upcase! if char =~ /[aeiou]/
+    char
+  end.join
 end
 
-p LetterChanges("Argument goes here")
+p letter_changes("Argument goes here")
