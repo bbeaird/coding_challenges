4880389c95527774cc73e005bb695c1e5c2dac3f
Brantley Beaird <bbeaird@gmail.com>
2014-06-17 14:59:24 -0700

    Determines whether or not sequence is arithmetic or geometric.

---
+++ arith_geo.rb
@@ -0,0 +1,24 @@
+  # get common difference and ratio of each term
+  # insert these into a hash
+  # return Arithmetic if all common differences are equal
+  # return Geometric if all common ratios are equal
+  # else return -1
+
+def arith_geo(arr)
+  common_differences = []
+  common_ratios = []
+  arr.each_with_index do |num, i|
+    if !(arr[i + 1].nil?)
+      common_differences << arr[i + 1] - arr[i]
+      common_ratios << arr[i + 1] / arr[i]
+    end
+  end
+
+  return "Arithmetic" if common_differences.uniq.length == 1
+  return "Geometric" if common_ratios.uniq.length == 1
+  return -1
+end
+
+p arith_geo([1,2,3,100])
+p arith_geo([1,2,3,4])
+p arith_geo([1,2,4,8])
