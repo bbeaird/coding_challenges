3e8db2739857e57f8a5e1c8baea7b85f572069d8
Brantley Beaird <bbeaird@gmail.com>
2014-06-16 17:06:42 -0700

    Checks a string to see if a and b are separated by 3 characters.

---
+++ ab_check.rb
@@ -0,0 +1,11 @@
+def ab_check(str)
+  return true if str.index(/[a]...[b]/)
+  return true if str.index(/[b]...[a]/)
+  return false
+end
+
+p ab_check("wwwwafffbwwww")
+p ab_check("wwwwa   bwwww")
+p ab_check("wwwwa 4 bwwww")
+p ab_check("wwwwa bwwww")
+p ab_check("wwwwa 555 bwwww")
