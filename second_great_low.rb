3842cecad4239b9483a24251fbb04caa1626ff8f
Brantley Beaird <bbeaird@gmail.com>
2014-06-21 16:36:37 -0700

    Deals with empty, 1, and 2 entry arrays. Also deals with duplicates in array like [1,1,1,2,3,4,100].

--- second_great_low.rb
+++ second_great_low.rb
@@ -1,10 +1,38 @@
-arr = [1, 2, 3, 4, 100]
+arr = [1, 1, 2, 3, 4, 100]
+# arr = [1, 3]
+
+# deal with empty, 1, 2, and 3 entry arrays.
+# deal with duplicates.
 
 def second_great_low(arr)
+  size = arr.size
+  raise "Empty array!" if size == 0
+  raise "Only 1 entry in array!" if size == 1
+  return "#{arr.first} #{arr.last}" if size == 2
+
   arr.sort!
-  arr.shift
-  arr.pop
-  return "#{arr.first} #{arr.last}"
+  smallest = arr.first
+  second_smallest = nil
+
+  arr.each do |num|
+    if num != smallest
+      second_smallest = num
+      break
+    end
+  end
+
+  arr.reverse!
+  largest = arr.first
+  second_largest = nil
+
+  arr.each do |num|
+    if num != largest
+      second_largest = num
+      break
+    end
+  end
+
+  return "#{second_smallest} #{second_largest}"
 end
 
 p second_great_low(arr)
