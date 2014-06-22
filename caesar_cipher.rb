4156e6ebb235b34c201812f61b99426fa8c8b088
Brantley Beaird <bbeaird@gmail.com>
2014-06-22 15:47:00 -0700

    Created a method that did not use the rotate method...probably better.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -6,7 +6,6 @@ def caesar_cipher(str, num)
   ciphered_string = ""
 
   str.each_char do |char|
-    p char
     if char =~ /[a-z]/
       ciphered_string += a_to_z_rotated[a_to_z.index(char)]
     elsif char =~ /[A-Z]/
@@ -15,8 +14,30 @@ def caesar_cipher(str, num)
       ciphered_string += char
     end
   end
+  p str
+  p "changes to.............."
+  return ciphered_string
+end
+
+def caesar_cipher_no_rotate_method(str, num)
+  a_to_z = ('a'..'z').to_a
+  capital_a_to_z = ('A'..'Z').to_a
+  ciphered_string = ""
+
+  str.each_char do |char|
+    if char =~ /[a-z]/
+      ciphered_string += a_to_z[(a_to_z.index(char) + num) % 26]
+    elsif char =~ /[A-Z]/
+      ciphered_string += capital_a_to_z[(capital_a_to_z.index(char) + num) % 26]
+    else
+      ciphered_string += char
+    end
+  end
+  p str
+  p "changes to.............."
   return ciphered_string
 end
 
 str = "Hello Brantley"
 p caesar_cipher(str, 2)
+p caesar_cipher_no_rotate_method(str, 2)
