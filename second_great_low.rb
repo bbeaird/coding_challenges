5e612a8eb979108880facac59f60cce8819ab713
Brantley Beaird <bbeaird@gmail.com>
2014-06-21 17:15:26 -0700

    Refactored using uniq method and general slimming down.

--- second_great_low.rb
+++ second_great_low.rb
@@ -1,9 +1,3 @@
-arr = [1, 1, 2, 3, 4, 100]
-# arr = [1, 3]
-
-# deal with empty, 1, 2, and 3 entry arrays.
-# deal with duplicates.
-
 def second_great_low(arr)
   size = arr.size
   raise "Empty array!" if size == 0
@@ -35,4 +29,22 @@ def second_great_low(arr)
   return "#{second_smallest} #{second_largest}"
 end
 
-p second_great_low(arr)
+# make a method using uniq
+# make a method using reject?
+
+def second_great_low_uniq(arr)
+  size = arr.size
+  raise "Empty array!" if size == 0
+  raise "Only 1 entry in array!" if size == 1
+  return "#{arr.first} #{arr.last}" if size == 2
+
+  arr.sort!.uniq!
+  return "#{arr[1]} #{arr[-2]}"
+end
+
+arr = [1, 1, 2, 3, 4, 100, 19, 18]
+
+# p second_great_low(arr)
+# p second_great_low([1,5])
+p second_great_low_uniq(arr)
+p second_great_low_uniq([1, 5])
