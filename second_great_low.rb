c04eb5e2d018bee1ae6cbd8797cfc74526886a30
Brantley Beaird <bbeaird@gmail.com>
2014-06-21 17:28:49 -0700

    Added a method that uses reject to find the second highest and second lowest numbers.

--- second_great_low.rb
+++ second_great_low.rb
@@ -29,9 +29,6 @@ def second_great_low(arr)
   return "#{second_smallest} #{second_largest}"
 end
 
-# make a method using uniq
-# make a method using reject?
-
 def second_great_low_uniq(arr)
   size = arr.size
   raise "Empty array!" if size == 0
@@ -42,9 +39,25 @@ def second_great_low_uniq(arr)
   return "#{arr[1]} #{arr[-2]}"
 end
 
+def second_great_low_via_reject(arr)
+  size = arr.size
+  raise "Empty array!" if size == 0
+  raise "Only 1 entry in array!" if size == 1
+  return "#{arr.first} #{arr.last}" if size == 2
+
+  arr.sort!
+  max = arr.last
+  min = arr.first
+  arr.reject! { |x| x == max || x == min }
+  return "#{arr.first} #{arr.last}"
+end
+
 arr = [1, 1, 2, 3, 4, 100, 19, 18]
 
-# p second_great_low(arr)
-# p second_great_low([1,5])
+p second_great_low(arr)
+p second_great_low([1,5])
 p second_great_low_uniq(arr)
 p second_great_low_uniq([1, 5])
+p second_great_low_via_reject(arr)
+p second_great_low_via_reject([1, 5])
+
