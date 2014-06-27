8aa2982efca4343e5dfa29ac3e497e006ce388d0
Brantley Beaird <bbeaird@gmail.com>
2014-06-26 19:28:46 -0700

    Now adds commas in appropriate places for numbers 1,000 and greater.

--- formatted_division.rb
+++ formatted_division.rb
@@ -35,14 +35,27 @@ def formatted_division_no_round_method(num1, num2)
   until result_split_on_decimal[1].length == 4
     result_split_on_decimal[1] += '0'
   end
+  nums_after_decimal = result_split_on_decimal[1]
 
-  formatted_result = result_split_on_decimal.join('.')
+  nums_before_decimal = result_split_on_decimal[0].split('')
+  nums_before_decimal.reverse!
+  nums_before_decimal.map!.with_index do |num, i|
+     (i + 4) % 3 == 0 ? num = ',' + num : num
+  end
+  nums_before_decimal.reverse!
+  nums_before_decimal = nums_before_decimal.join('')
+
+  formatted_result = nums_before_decimal + '.' + nums_after_decimal
   return formatted_result
 end
 
-# p formatted_division(2, 3) # "0.6667"
-# p formatted_division(10, 3) # "3.3333"
-# p formatted_division(50, 3) # "3.3333"
+def formatted_division2(num1, num2)
+
+end
+
+p formatted_division(2, 3) # "0.6667"
+p formatted_division(10, 3) # "3.3333"
+p formatted_division(50, 3) # "3.3333"
 
 p formatted_division_no_round_method(2, 3) # "0.6667"
 p formatted_division_no_round_method(10, 3) # "3.3333"
@@ -52,3 +65,4 @@ p formatted_division_no_round_method(9112, 2)
 p formatted_division_no_round_method(5, 2)
 p formatted_division_no_round_method(2, 4)
 p formatted_division_no_round_method(9, 8)
+p formatted_division_no_round_method(101077282, 21123)
