88937d49de112495215537c78f9319db81753859
Brantley Beaird <bbeaird@gmail.com>
2014-06-28 18:24:45 -0700

    Made a shorter version that uses the String scan method.

--- number_addition.rb
+++ number_addition.rb
@@ -1,5 +1,10 @@
 def number_addition(str)
-  str.split(/\D/).select { |num| !(num.empty?) }.map { |num| num.to_i }.inject(:+)
+  str.split(/\D/).select { |num| !(num.empty?) }.map(&:to_i).inject(:+)
+end
+
+def number_addition2(str)
+  str.scan(/\d{1,}/).map(&:to_i).inject(:+)
 end
 
 p number_addition("88Hello 3World!")
+p number_addition2("88Hello 3World!")
