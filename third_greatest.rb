e8d7301b88286fcda10115e336d99172c8ed33a7
Brantley Beaird <bbeaird@gmail.com>
2014-06-29 18:44:27 -0700

    Fixed it so that it sorts in descending order and returns the third longest string.

--- third_greatest.rb
+++ third_greatest.rb
@@ -3,4 +3,13 @@ def third_greatest(str_arr)
   str_arr[-3]
 end
 
-p third_greatest(["abc","defg","z","hijk"])
+def third_greatest2(str_arr)
+  str_arr = str_arr.sort { |x, y| y.length <=> x.length }
+  str_arr[2]
+end
+
+# p third_greatest(["abc","defg","z","hijk"])
+p third_greatest2(["abc","defg","z","hijk"])
+p third_greatest2(["coder","byte","code"])
+p third_greatest2(["hello","world","world"])
+p third_greatest2(["bt","ct","mtyyyy"])
