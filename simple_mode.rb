317b14e324de358a2054144e69997c87d384f1b4
Brantley Beaird <bbeaird@gmail.com>
2014-07-08 20:22:20 -0700

    Now keeps track of max while iterating over array. Makes is so that it doesn't have to search through the entire hash at the end to find the max.

--- simple_mode.rb
+++ simple_mode.rb
@@ -1,9 +1,11 @@
 def simple_mode_with_hash(arr)
   count_hash = Hash.new(0)
+  max = 0
   arr.each do |num|
     count_hash[num] += 1
+    max = count_hash[num] if count_hash[num] > max
   end
-  return -1 if count_hash.values.max == 1
+  return -1 if max == 1
   count_hash.max_by { |k, v| v }[0]
 end
 
