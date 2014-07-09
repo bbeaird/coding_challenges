594a8df70954643fb4d3b5426b27a64500a7be99
Brantley Beaird <bbeaird@gmail.com>
2014-07-08 20:13:34 -0700

    Now returns -1 if there is no mode.

--- simple_mode.rb
+++ simple_mode.rb
@@ -3,8 +3,10 @@ def simple_mode_with_hash(arr)
   arr.each do |num|
     count_hash[num] += 1
   end
+  return -1 if count_hash.values.max == 1
   count_hash.max_by { |k, v| v }[0]
 end
 
 arr = [5,5,2,2,2,1,5]
 p simple_mode_with_hash(arr)
+p simple_mode_with_hash([7,8,1,2,3,4,89,94,109,76])
