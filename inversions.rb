71bccabf965ca93920b5269065dce5ff0387ebdc
Brantley Beaird <bbeaird@gmail.com>
2014-07-13 18:03:33 -0700

    Naive count of inversions in array. O(n^2).

---
+++ inversions.rb
@@ -0,0 +1,12 @@
+def count_inversions_naive(arr)
+  count = 0
+  arr.each do |i|
+    arr.each do |j|
+      count += 1 if j > i
+    end
+  end
+  return count
+end
+
+arr = [6,5,4,3,2,1]
+p count_inversions_naive(arr)
