bf43d1cfa905f22c4f2c7b0206700abe5314137f
Brantley Beaird <bbeaird@gmail.com>
2014-07-18 14:15:39 -0700

    Accounts for num = 1. 1 is not prime.

--- prime_checker.rb
+++ prime_checker.rb
@@ -2,12 +2,13 @@ def prime_checker(num)
   split_num = num.to_s.split("")
   permutations = split_num.permutation.to_a
   permutations.each do |p|
-    return true if is_this_prime?(p.join("").to_i)
+    return 1 if is_this_prime?(p.join("").to_i)
   end
-  return false
+  return 0
 end
 
 def is_this_prime?(num)
+  return false if num == 1
   for i in 2..num**0.5
     return false if num % i == 0
   end
@@ -19,3 +20,7 @@ p prime_checker(18)
 
 # p is_this_prime?(18)
 # p is_this_prime?(19)
+# p is_this_prime?(2)
+# p is_this_prime?(1)
+# p is_this_prime?(3)
+# p is_this_prime?(4)
