09398e673fd73fbcc68613786ebe160efa8a3192
Brantley Beaird <bbeaird@gmail.com>
2014-07-25 17:09:57 -0700

    Swaps case of all letters. Also if a letter is between two numbers (without separation), the numbers are swapped. That was challenging.

---
+++ swap2.rb
@@ -0,0 +1,27 @@
+def swap2(str)
+  modified_str = ""
+  digit_letters_digit = str.scan(/\d[a-zA-Z]{1,}\d/)
+  digit_letters_digit.each do |w|
+    first_digit = w[0]
+    last_digit = w[-1]
+    replacement = w.dup
+    replacement[0] = last_digit
+    replacement[-1] = first_digit
+    str.gsub!(w, replacement)
+  end
+
+  str.each_char do |char|
+    if char =~ /[a-zA-Z]/
+      if char == char.upcase
+        modified_str += char.downcase
+      else
+        modified_str += char.upcase
+      end
+    else
+      modified_str += char
+    end
+  end
+  modified_str
+end
+
+p swap2("2S 6 du5d4e 234eee6")
