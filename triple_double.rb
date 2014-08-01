6ab77482034fac402f9dc5ef3a4147634b72a136
Brantley Beaird <bbeaird@gmail.com>
2014-08-01 14:00:14 -0700

    Checks first number for triples. If second number contains a double of at least one of the triples from the first number, return 1, else return 0.

---
+++ triple_double.rb
@@ -0,0 +1,37 @@
+def triple_double(num1, num2)
+  triples = []
+  split_num1 = num1.to_s.split('')
+  prevprev1 = split_num1[0]
+  prev1 = split_num1[1]
+
+  for i in 2..(split_num1.length - 1)
+    curr1 = split_num1[i]
+    if curr1 == prev1 && prev1 == prevprev1
+      triples << curr1
+    end
+    prevprev1 = split_num1[i - 1]
+    prev1 = split_num1[i]
+  end
+
+  doubles = []
+  split_num2 = num2.to_s.split('')
+  prev2 = split_num2[0]
+
+  for i in 1..(split_num2.length - 1)
+    curr2 = split_num2[i]
+    if curr2 == prev2
+      doubles << curr2
+    end
+    prev2 = split_num2[i]
+  end
+
+  if !triples.empty?
+    triples.each do |i|
+      return 1 if doubles.include?(i)
+    end
+  end
+  return 0
+end
+
+p triple_double(465555, 5579)
+p triple_double(67844, 66237)
