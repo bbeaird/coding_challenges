cd64b27aa6d9c1e001a9621b99cbcd95007f7d01
Brantley Beaird <bbeaird@gmail.com>
2014-08-03 16:46:09 -0700

    Did some minor refactoring on string_reduction.

--- string_reduction.rb
+++ string_reduction.rb
@@ -3,14 +3,12 @@ def string_reduction(str)
   for i in 1..(str.length - 1)
     prev = str[i - 1]
     curr = str[i]
-    first_two_chars = [prev, curr]
-    diff = abc - first_two_chars
+    chars_to_compare = [prev, curr]
+    diff = abc - chars_to_compare
     case
       when diff.length == 1
         str.slice!(i - 1, 2)
         str.prepend(diff.first)
-        p 'x'*50
-        p str
         string_reduction(str)
     end
     prev = str[i]
@@ -19,11 +17,12 @@ def string_reduction(str)
 end
 
 p string_reduction("abcabc") # should return 2
-p string_reduction("a")
 # abcabc
 # ccabc
 # cbbc
 # abc
 # cc
 
+p string_reduction("a") # should return 1
 p string_reduction("cccc") # should return 4
+p string_reduction("abc") # should return 2
