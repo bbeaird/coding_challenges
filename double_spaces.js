21b1b37b4eada3cdfa1bdd336485c5d050aa981e
Brantley Beaird <bbeaird@gmail.com>
2014-08-16 16:55:38 -0700

    Checks a string for double spaces.

---
+++ double_spaces.js
@@ -0,0 +1,20 @@
+// var checkForDoubleSpaces = function () {
+
+// }
+
+function checkForDoubleSpaces(string) {
+  var doubleSpacesFlag = false;
+  for (i = 0; i < string.length; i++) {
+    if (string[i] === " " && string[i-1] === " ") {
+      alert("You have double spaces in your string!");
+      doubleSpacesFlag = true;
+      break;
+    }
+  }
+  if (doubleSpacesFlag === false) {
+    alert("There are no double spaces in your string!");
+  }
+}
+
+checkForDoubleSpaces("My name is Brantley!");
+checkForDoubleSpaces("My  name is Brantley!");
