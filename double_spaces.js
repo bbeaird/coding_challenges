5bb860e11eec2a391332fbfab764999b8ecd495d
Brantley Beaird <bbeaird@gmail.com>
2014-08-16 16:58:31 -0700

    Checks for double spaces as a function as well as...as a variable.

--- double_spaces.js
+++ double_spaces.js
@@ -1,8 +1,18 @@
-// var checkForDoubleSpaces = function () {
-
-// }
-
 function checkForDoubleSpaces(string) {
+  var doubleSpacesFlag = false;
+  for (i = 0; i < string.length; i++) {
+    if (string[i] === " " && string[i-1] === " ") {
+      alert("You have double spaces in your string!");
+      doubleSpacesFlag = true;
+      break;
+    }
+  }
+  if (doubleSpacesFlag === false) {
+    alert("There are no double spaces in your string!");
+  }
+}
+
+var checkForDoubleSpacesVar = function(string) {
   var doubleSpacesFlag = false;
   for (i = 0; i < string.length; i++) {
     if (string[i] === " " && string[i-1] === " ") {
@@ -18,3 +28,6 @@ function checkForDoubleSpaces(string) {
 
 checkForDoubleSpaces("My name is Brantley!");
 checkForDoubleSpaces("My  name is Brantley!");
+
+checkForDoubleSpacesVar("My name is Brantley!");
+checkForDoubleSpacesVar("My  name is Brantley!");
