28dd5aa3c055e5e79358accc2673ffe3fb836ff2
Brantley Beaird <bbeaird@gmail.com>
2014-08-17 17:31:31 -0700

    Successfully used slice to replace World War II with the Second World War.

--- WWII.js
+++ WWII.js
@@ -1,17 +1,15 @@
 var newYorkerText = "Back in World War II, there were lots of soliders. Many of the soldiers in World War II were killed. It is sad that so many people died in World War II."
 
 function replaceWWII(string) {
-  var newString = "";
-  for (var firstChar = 0; firstChar <= -1; firstChar++) {
-    console.log("Inside for loop");
-    firstChar = string.indexOf("World War II");
-    console.log(firstChar);
+  var newString = string.slice(0);
+  var firstChar = 0;
+  while (firstChar !== -1) {
+    firstChar = newString.indexOf("World War II");
     if (firstChar !== -1) {
-      newString = string.slice(0, firstChar) + " the Second World War " + string.slice(firstChar + 1);
-      console.log(newString);
+      newString = newString.slice(0, firstChar) + "the Second World War" + newString.slice(firstChar + 12);
     }
   }
-  return newString;
+  console.log(newString);
 }
 
 replaceWWII(newYorkerText);
