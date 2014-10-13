322d5eb776beba9ba3bc224e53795b1f9ff68f32
Brantley Beaird <bbeaird@gmail.com>
2014-10-12 20:50:26 -0700

    Used my_each to create my_times...that was pretty awesome Brantley...

--- my_each.rb
+++ my_each.rb
@@ -10,3 +10,16 @@ class Array
 end
 
 [10, 20, 30].my_each { |x| puts "Here is each array value squared...#{x**2}!!!" }
+
+# now let's implement my_times using my_each!!!!!!!!!!!
+
+class Integer
+  def my_times
+    (0..(self - 1)).to_a.my_each do |i|
+      yield(i)
+    end
+    self
+  end
+end
+
+5.my_times { |i| puts "my_times implementation on iteration number #{i}!!!"}
