25693d37e2ebd9bf3feab1df0abd0fd0273f5b98
Brantley Beaird <bbeaird@gmail.com>
2014-10-12 20:58:07 -0700

    Created a my_map function all on my own...go me.

--- my_each.rb
+++ my_each.rb
@@ -10,6 +10,7 @@ class Array
 end
 
 [10, 20, 30].my_each { |x| puts "Here is each array value squared...#{x**2}!!!" }
+p [10, 20, 30].my_each { |x| puts "Here is each array value squared...#{x**2}!!!" }
 
 # now let's implement my_times using my_each!!!!!!!!!!!
 
@@ -23,3 +24,19 @@ class Integer
 end
 
 5.my_times { |i| puts "my_times implementation on iteration number #{i}!!!"}
+p 5.my_times { |i| puts "my_times implementation on iteration number #{i}!!!"}
+
+class Array
+  def my_map
+    c = 0
+    return_value = []
+    until c == self.size
+      return_value << yield(self[c])
+      c += 1
+    end
+    return_value
+  end
+end
+
+[5, 10, 15].my_map { |n| p n**3 }
+p [5, 10, 15].my_map { |n| p n**3 }
